e1eb45f9d901ba9c7e873287338b13d317d7938c
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-05 11:08:55 +0300

    added middleware cors

--- main.py
+++ main.py
@@ -5,15 +5,26 @@ from routes.users import router as users_router
 from routes.auth import router as auth_router
 from routes.products import router as products_router
 
-
+from fastapi.middleware.cors import CORSMiddleware  
 app = FastAPI()
 app.include_router(users_router)
 app.include_router(auth_router)
 app.include_router(products_router)
 
+origins = [
+    "http://localhost.tiangolo.com",
+    "https://localhost.tiangolo.com",
+    "http://localhost",
+    "http://localhost:8080",
+]
 
-
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 
 @app.get("/")
