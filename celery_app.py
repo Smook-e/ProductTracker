fd0f33296464d1f3e0efb0dde3d908a120893c4d
skedd <es.ahmed.nasser20231@alexu.edu.eg>
2026-06-09 16:16:43 +0300

    updated scraper to handle a clean amazon url

--- celery_app.py
+++ celery_app.py
@@ -10,17 +10,17 @@ celery_app = Celery(
     "worker",
     broker=REDIS_URL,
     backend=REDIS_URL,
-    include=["worker.tasks"]
+    include=["worker.tasks", "worker.scheduler"]
 )
 
 celery_app.conf.update(
     task_track_started=True,
     worker_prefetch_multiplier=1
 )
-# 🚀 ADD THE BEAT SCHEDULE HERE:
+
 celery_app.conf.beat_schedule = {
     "auto-scrape-expired-products-every-5-minutes": {
-        "task": "worker.tasks.check_expired_products", # Name of our task
-        "schedule": 300.0, # Run every 300 seconds (5 minutes)
+        "task": "worker.scheduler.check_expired_products", # Name of our task
+        "schedule": 30.0, # Run every 300 seconds (5 minutes)
     },
 }
